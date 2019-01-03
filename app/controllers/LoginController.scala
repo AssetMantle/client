77f90e6169421a103df29c9ced1e69014fd08b0e
Deepanshu Tripathi <deepanshu@cosmiclabs.co>
2019-01-03 21:00:57 +0530

    Added login controller.

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import akka.actor.ActorSystem
 import javax.inject.Inject
-import models.{Account, Accounts}
+import models.master.{Account, Accounts}
 import play.api.mvc.{MessagesAbstractController, MessagesControllerComponents}
 
 import scala.concurrent.ExecutionContext
@@ -10,6 +10,6 @@ import scala.concurrent.ExecutionContext
 class LoginController @Inject()(messagesControllerComponents: MessagesControllerComponents, actorSystem: ActorSystem, accounts: Accounts)(implicit exec: ExecutionContext) extends MessagesAbstractController(messagesControllerComponents) {
 
   def login = Action.async {
-    accounts.add(Account("a", "b", 1, "d", "e", "s", "s")).map(x => Ok(s"$x"))
+    accounts.add(Account("a", "b", "a")).map(x => Ok(s"$x"))
   }
 }
