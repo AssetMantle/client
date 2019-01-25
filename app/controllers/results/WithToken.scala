910ab769d65d2d59f3ed1f6195d5af3cbdcd096a
Deepanshu Tripathi <deepanshu@cosmiclabs.co>
2019-01-25 15:34:57 +0530

    Added flash and index success and failure messages and common html handling.

--- app/controllers/results/WithToken.scala
+++ app/controllers/results/WithToken.scala
@@ -7,5 +7,5 @@ import play.api.http.Writeable
 import play.api.mvc.{RequestHeader, Result, Results}
 
 class WithToken @Inject()(accounts: Accounts) {
-  def Ok[C](content: C)(implicit request: RequestHeader, writeable: Writeable[C]): Result = Results.Ok(content).withSession(request.session - Security.token + (Security.token -> accounts.Service.refreshToken(request.session.get(Security.username).getOrElse("ss"))))
+  def Ok[C](content: C)(implicit request: RequestHeader, writeable: Writeable[C]): Result = Results.Ok(content).withSession(request.session - Security.TOKEN + (Security.USERNAME -> accounts.Service.refreshToken(request.session.get(Security.USERNAME).getOrElse("ss"))))
 }
