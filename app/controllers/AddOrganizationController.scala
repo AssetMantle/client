16d4de06b5ac223e1f038619c5808cbd46be3065
deepanshutr <deepanshu@cosmiclabs.co>
2019-02-22 19:22:39 +0530

    Added master add organization, and renamed schema.

--- app/controllers/AddOrganizationController.scala
+++ app/controllers/AddOrganizationController.scala
@@ -1,7 +1,9 @@
 package controllers
 
+import controllers.actions.WithLoginAction
 import exceptions.{BaseException, BlockChainException}
 import javax.inject.Inject
+import models.master.Organizations
 import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc.{AbstractController, Action, AnyContent, MessagesControllerComponents}
 import transactions.AddOrganization
@@ -9,20 +11,20 @@ import views.companion.{blockchain, master}
 
 import scala.concurrent.ExecutionContext
 
-class AddOrganizationController @Inject()(messagesControllerComponents: MessagesControllerComponents, transactionAddOrganization: AddOrganization)(implicit exec: ExecutionContext) extends AbstractController(messagesControllerComponents) with I18nSupport {
+class AddOrganizationController @Inject()(messagesControllerComponents: MessagesControllerComponents, transactionAddOrganization: AddOrganization, organizations: Organizations, withLoginAction: WithLoginAction)(implicit exec: ExecutionContext) extends AbstractController(messagesControllerComponents) with I18nSupport {
 
   def addOrganizationForm: Action[AnyContent] = Action { implicit request =>
     Ok(views.html.component.master.addOrganization(master.AddOrganization.form))
   }
 
-  def addOrganization: Action[AnyContent] = Action { implicit request =>
+  def addOrganization: Action[AnyContent] = withLoginAction { implicit request =>
     master.AddOrganization.form.bindFromRequest().fold(
       formWithErrors => {
         BadRequest(views.html.component.master.addOrganization(formWithErrors))
       },
       addOrganizationData => {
         try {
-          Ok(views.html.index(success = transactionAddOrganization.Service.post(new transactionAddOrganization.Request(addOrganizationData.from, addOrganizationData.to, addOrganizationData.organizationID, addOrganizationData.chainID, addOrganizationData.password)).txHash))
+          Ok(views.html.index(success = organizations.Service.addOrganization(request.session.get(constants.Security.USERNAME).get, addOrganizationData.name, addOrganizationData.address, addOrganizationData.phone, addOrganizationData.email)))
         }
         catch {
           case baseException: BaseException => Ok(views.html.index(failure = Messages(baseException.message)))
--- app/views/companion/master/AddOrganization.scala
+++ app/views/companion/master/AddOrganization.scala
@@ -8,8 +8,6 @@ object AddOrganization {
 
   val form = Form(
     mapping(
-      "id" -> nonEmptyText(minLength = 4, maxLength = 20),
-      "password" -> nonEmptyText(minLength = 8, maxLength = 50),
       "name" -> nonEmptyText(minLength = 8, maxLength = 50),
       "address" -> nonEmptyText(minLength = 8, maxLength = 50),
       "phone" -> nonEmptyText(minLength = 8, maxLength = 50),
@@ -18,6 +16,6 @@ object AddOrganization {
     )(Data.apply)(Data.unapply)
   )
 
-  case class Data(id: String, password: String, name: String, address: String, phone: String, email: String)
+  case class Data(name: String, address: String, phone: String, email: String)
 
 }
