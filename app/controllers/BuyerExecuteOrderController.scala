9245cb20d5ca12d7d38d8b66fac39f22928da015
luffy632 <abhinav@cosmiclabs.co>
2019-03-17 00:06:50 +0530

    sendCoin, BuyerExecuteOrder witLoginAction, pickup username from session and address from db

--- app/controllers/BuyerExecuteOrderController.scala
+++ app/controllers/BuyerExecuteOrderController.scala
@@ -1,24 +1,56 @@
 package controllers
 
+import controllers.actions.WithLoginAction
 import exceptions.{BaseException, BlockChainException}
 import javax.inject.Inject
 import models.blockchainTransaction.BuyerExecuteOrders
+import models.master.Accounts
 import play.api.Configuration
 import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc.{AbstractController, Action, AnyContent, MessagesControllerComponents}
-import views.companion.blockchain.BuyerExecuteOrder
+import views.companion.{blockchain, master}
 
 import scala.concurrent.ExecutionContext
 import scala.util.Random
 
-class BuyerExecuteOrderController @Inject()(messagesControllerComponents: MessagesControllerComponents, transactionBuyerExecuteOrder: transactions.BuyerExecuteOrder, buyerExecuteOrders: BuyerExecuteOrders)(implicit exec: ExecutionContext,configuration: Configuration) extends AbstractController(messagesControllerComponents) with I18nSupport {
+class BuyerExecuteOrderController @Inject()(messagesControllerComponents: MessagesControllerComponents, withLoginAction: WithLoginAction, transactionBuyerExecuteOrder: transactions.BuyerExecuteOrder, buyerExecuteOrders: BuyerExecuteOrders)(implicit exec: ExecutionContext,configuration: Configuration, accounts: Accounts) extends AbstractController(messagesControllerComponents) with I18nSupport {
 
   def buyerExecuteOrderForm: Action[AnyContent] = Action { implicit request =>
-    Ok(views.html.component.blockchain.buyerExecuteOrder(BuyerExecuteOrder.form))
+    Ok(views.html.component.master.buyerExecuteOrder(master.BuyerExecuteOrder.form))
   }
 
-  def buyerExecuteOrder: Action[AnyContent] = Action { implicit request =>
-    BuyerExecuteOrder.form.bindFromRequest().fold(
+  def buyerExecuteOrder: Action[AnyContent] = withLoginAction { implicit request =>
+    master.BuyerExecuteOrder.form.bindFromRequest().fold(
+      formWithErrors => {
+        BadRequest(views.html.component.master.buyerExecuteOrder(formWithErrors))
+      },
+      buyerExecuteOrderData => {
+        try {
+          if (configuration.get[Boolean]("blockchain.kafka.enabled")) {
+            val response = transactionBuyerExecuteOrder.Service.kafkaPost( transactionBuyerExecuteOrder.Request(from = request.session.get(constants.Security.USERNAME).get, password = buyerExecuteOrderData.password, buyerAddress = accounts.Service.getId(request.session.get(constants.Security.USERNAME).get), sellerAddress = buyerExecuteOrderData.sellerAddress, fiatProofHash = buyerExecuteOrderData.fiatProofHash, pegHash = buyerExecuteOrderData.pegHash, gas = buyerExecuteOrderData.gas))
+            buyerExecuteOrders.Service.addBuyerExecuteOrderKafka(from = request.session.get(constants.Security.USERNAME).get, buyerAddress = accounts.Service.getId(request.session.get(constants.Security.USERNAME).get), sellerAddress = buyerExecuteOrderData.sellerAddress, fiatProofHash = buyerExecuteOrderData.fiatProofHash, pegHash = buyerExecuteOrderData.pegHash, gas = buyerExecuteOrderData.gas, null, null, ticketID = response.ticketID, null)
+            Ok(views.html.index(success = response.ticketID))
+          } else {
+            val response = transactionBuyerExecuteOrder.Service.post( transactionBuyerExecuteOrder.Request(from = request.session.get(constants.Security.USERNAME).get, password = buyerExecuteOrderData.password, buyerAddress = accounts.Service.getId(request.session.get(constants.Security.USERNAME).get), sellerAddress = buyerExecuteOrderData.sellerAddress, fiatProofHash = buyerExecuteOrderData.fiatProofHash, pegHash = buyerExecuteOrderData.pegHash, gas = buyerExecuteOrderData.gas))
+            buyerExecuteOrders.Service.addBuyerExecuteOrder(from = request.session.get(constants.Security.USERNAME).get, buyerAddress = accounts.Service.getId(request.session.get(constants.Security.USERNAME).get), sellerAddress = buyerExecuteOrderData.sellerAddress, fiatProofHash = buyerExecuteOrderData.fiatProofHash, pegHash = buyerExecuteOrderData.pegHash, gas = buyerExecuteOrderData.gas, null, txHash = Option(response.TxHash), ticketID = (Random.nextInt(899999999) + 100000000).toString, null)
+            Ok(views.html.index(success = response.TxHash))
+          }
+        }
+        catch {
+          case baseException: BaseException => Ok(views.html.index(failure = Messages(baseException.message)))
+          case blockChainException: BlockChainException => Ok(views.html.index(failure = blockChainException.message))
+
+        }
+      }
+    )
+  }
+
+  def buyerExecuteOrderFormBC: Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.component.blockchain.buyerExecuteOrder(blockchain.BuyerExecuteOrder.form))
+  }
+
+  def buyerExecuteOrderBC: Action[AnyContent] = Action { implicit request =>
+    blockchain.BuyerExecuteOrder.form.bindFromRequest().fold(
       formWithErrors => {
         BadRequest(views.html.component.blockchain.buyerExecuteOrder(formWithErrors))
       },
@@ -39,6 +71,7 @@ class BuyerExecuteOrderController @Inject()(messagesControllerComponents: Messag
           case blockChainException: BlockChainException => Ok(views.html.index(failure = blockChainException.message))
 
         }
-      })
+      }
+    )
   }
 }
