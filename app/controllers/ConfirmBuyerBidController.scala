35c055bc652bc7f21f994d95ab5d42b30e5e65b7
luffy632 <abhinav@cosmiclabs.co>
2019-03-25 17:30:45 +0530

    master/confirmBuyerBid withLoginAction

--- app/controllers/ConfirmBuyerBidController.scala
+++ app/controllers/ConfirmBuyerBidController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import controllers.actions.WithLoginAction
 import exceptions.{BaseException, BlockChainException}
 import javax.inject.Inject
 import models.blockchainTransaction.ConfirmBuyerBids
@@ -7,11 +8,42 @@ import play.api.Configuration
 import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc.{AbstractController, Action, AnyContent, MessagesControllerComponents}
 import views.companion.blockchain.ConfirmBuyerBid
+import views.companion.master
 
 import scala.concurrent.ExecutionContext
 import scala.util.Random
 
-class ConfirmBuyerBidController @Inject()(messagesControllerComponents: MessagesControllerComponents, transactionConfirmBuyerBid: transactions.ConfirmBuyerBid, confirmBuyerBids: ConfirmBuyerBids)(implicit exec: ExecutionContext, configuration: Configuration) extends AbstractController(messagesControllerComponents) with I18nSupport {
+class ConfirmBuyerBidController @Inject()(messagesControllerComponents: MessagesControllerComponents, withLoginAction: WithLoginAction, transactionConfirmBuyerBid: transactions.ConfirmBuyerBid, confirmBuyerBids: ConfirmBuyerBids)(implicit exec: ExecutionContext, configuration: Configuration) extends AbstractController(messagesControllerComponents) with I18nSupport {
+
+  def confirmBuyerBidForm: Action[AnyContent] = withLoginAction { implicit request =>
+    Ok(views.html.component.master.confirmBuyerBid(master.ConfirmBuyerBid.form))
+  }
+
+  def confirmBuyerBid: Action[AnyContent] = withLoginAction { implicit request =>
+    master.ConfirmBuyerBid.form.bindFromRequest().fold(
+      formWithErrors => {
+        BadRequest(views.html.component.master.confirmBuyerBid(formWithErrors))
+      },
+      confirmBuyerBidData => {
+        try {
+          if (configuration.get[Boolean]("blockchain.kafka.enabled")) {
+            val response = transactionConfirmBuyerBid.Service.kafkaPost( transactionConfirmBuyerBid.Request(from = request.session.get(constants.Security.USERNAME).get, to = confirmBuyerBidData.to, password = confirmBuyerBidData.password, bid = confirmBuyerBidData.bid, time = confirmBuyerBidData.time, pegHash = confirmBuyerBidData.pegHash, gas = confirmBuyerBidData.gas))
+            confirmBuyerBids.Service.addConfirmBuyerBidKafka(from = request.session.get(constants.Security.USERNAME).get, to = confirmBuyerBidData.to, bid = confirmBuyerBidData.bid, time = confirmBuyerBidData.time, pegHash = confirmBuyerBidData.pegHash, gas = confirmBuyerBidData.gas, null, null, ticketID = response.ticketID, null)
+            Ok(views.html.index(success = response.ticketID))
+          } else {
+            val response = transactionConfirmBuyerBid.Service.post( transactionConfirmBuyerBid.Request(from = request.session.get(constants.Security.USERNAME).get, to = confirmBuyerBidData.to, password = confirmBuyerBidData.password,  bid = confirmBuyerBidData.bid, time = confirmBuyerBidData.time, pegHash = confirmBuyerBidData.pegHash, gas = confirmBuyerBidData.gas))
+            confirmBuyerBids.Service.addConfirmBuyerBid(from = request.session.get(constants.Security.USERNAME).get, to = confirmBuyerBidData.to, bid = confirmBuyerBidData.bid, time = confirmBuyerBidData.time, pegHash = confirmBuyerBidData.pegHash, gas = confirmBuyerBidData.gas, null, txHash = Option(response.TxHash), ticketID = (Random.nextInt(899999999) + 100000000).toString, null)
+            Ok(views.html.index(success = response.TxHash))
+          }
+        }
+        catch {
+          case baseException: BaseException => Ok(views.html.index(failure = Messages(baseException.message)))
+          case blockChainException: BlockChainException => Ok(views.html.index(failure = blockChainException.message))
+
+        }
+      }
+    )
+  }
 
   def blockchainConfirmBuyerBidForm: Action[AnyContent] = Action { implicit request =>
     Ok(views.html.component.blockchain.confirmBuyerBid(ConfirmBuyerBid.form))
@@ -39,6 +71,7 @@ class ConfirmBuyerBidController @Inject()(messagesControllerComponents: Messages
           case blockChainException: BlockChainException => Ok(views.html.index(failure = blockChainException.message))
 
         }
-      })
+      }
+    )
   }
 }
---
+++ app/views/companion/master/ConfirmBuyerBid.scala
@@ -0,0 +1,20 @@
+package views.companion.master
+
+import play.api.data.Form
+import play.api.data.Forms._
+
+object ConfirmBuyerBid {
+  val form = Form(
+    mapping(
+      "password" -> nonEmptyText(minLength = 1, maxLength = 20),
+      "to" -> nonEmptyText(minLength = 1, maxLength = 45),
+      "bid" -> number(min = 1, max = 10000),
+      "time" -> number(min = 1, max = 10000),
+      "pegHash" -> nonEmptyText(minLength = 1, maxLength = 20),
+      "gas" -> number(min = 1, max = 10000)
+    )(Data.apply)(Data.unapply)
+  )
+
+  case class Data(password: String, to: String, bid: Int, time: Int, pegHash: String, gas: Int)
+
+}
