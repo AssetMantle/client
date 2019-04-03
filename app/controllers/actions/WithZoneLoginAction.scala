636ca13fadefa68a85ac22e360bf5b5fdbf5606e
luffy632 <abhinav@cosmiclabs.co>
2019-04-03 15:39:56 +0530

    Merge branch 'master' of https://gitlab.com/deepanshutr/comdex into luffy632
    # Conflicts:
    #	app/constants/Forms.scala
    #	app/controllers/LoginController.scala
    #	app/views/companion/master/Login.scala
    #	app/views/companion/master/UpdateContact.scala

--- app/controllers/actions/WithZoneLoginAction.scala
+++ app/controllers/actions/WithZoneLoginAction.scala
@@ -10,7 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class WithZoneLoginAction  @Inject()(defaultBodyParse: BodyParsers.Default, masterAccounts: Accounts, accountTokens: AccountTokens)(implicit executionContext: ExecutionContext) extends ActionBuilderImpl(defaultBodyParse) {
   override def invokeBlock[T](request: Request[T], block: Request[T] => Future[Result]): Future[Result] = {
-    if (accountTokens.Service.verifySession(request.session.get(Security.USERNAME), request.session.get(Security.TOKEN)) && masterAccounts.Service.getUserType(request.session.get(Security.USERNAME).getOrElse(constants.User.UNKNOWN)) == Option(constants.User.ZONE)) {
+    if (accountTokens.Service.verifySession(request.session.get(Security.USERNAME), request.session.get(Security.TOKEN)) && masterAccounts.Service.getUserType(request.session.get(Security.USERNAME).getOrElse(constants.User.UNKNOWN)) == constants.User.ZONE) {
       block(request)
     }
     else {
