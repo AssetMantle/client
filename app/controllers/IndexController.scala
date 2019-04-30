00dfa1c3ad4f98a45d86e884aa3c61358658f305
luffy632 <abhinav@cosmiclabs.co>
2019-04-30 15:50:24 +0530

    index ui

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -5,8 +5,6 @@ import play.api.Configuration
 import play.api.i18n.I18nSupport
 import play.api.mvc.{AbstractController, Action, AnyContent, MessagesControllerComponents}
 
-import scala.concurrent.ExecutionContext
-
 class IndexController @Inject()(messagesControllerComponents: MessagesControllerComponents)(implicit configuration: Configuration) extends AbstractController(messagesControllerComponents) with I18nSupport {
 
   def index: Action[AnyContent] = Action { implicit request =>
