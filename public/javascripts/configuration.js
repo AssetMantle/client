38ef1dca04173df7647fff2b812d5ae4a7e411fd
luffy632 <abhinav@cosmiclabs.co>
2019-05-01 22:03:38 +0530

    index ui css

--- public/javascripts/configuration.js
+++ public/javascripts/configuration.js
@@ -1,8 +1,8 @@
 function getConfiguration(configuration) {
     let configurationValue = getCookie(configuration);
-    if (configurationValue !== ""){
+    if (configurationValue !== "") {
         return configurationValue;
-    }else {
+    } else {
         let route = jsRoutes.controllers.ConfigurationController.queryConfigurationVariable(configuration);
         $.ajax({
             url: route.url,
@@ -19,39 +19,21 @@ function getConfiguration(configuration) {
 }
 
 function getConfigurationAsynchronously(configuration) {
-    let route = jsRoutes.controllers.ConfigurationController.queryConfigurationVariable(configuration);
-    $.ajax({
-        url: route.url,
-        type: route.type,
-        async: true,
-        statusCode: {
-            200: function (data) {
-                setCookie(configuration, data, 1);
-                return getCookie(configuration);
+    let configurationValue = getCookie(configuration);
+    if (configurationValue !== "") {
+        return configurationValue;
+    } else {
+        let route = jsRoutes.controllers.ConfigurationController.queryConfigurationVariable(configuration);
+        $.ajax({
+            url: route.url,
+            type: route.type,
+            async: true,
+            statusCode: {
+                200: function (data) {
+                    setCookie(configuration, data, 1);
+                    return getCookie(configuration);
+                }
             }
-        }
-    });
-}
-
-function getCookie(cookieName) {
-    var name = cookieName + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
+        });
     }
-    return "";
-}
-
-function setCookie(cookieName, cookieValue, expiryDays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (expiryDays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
 }
---
+++ public/javascripts/cookie.js
@@ -0,0 +1,22 @@
+function getCookie(cookieName) {
+    let name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cookieName, cookieValue, expiryDays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
