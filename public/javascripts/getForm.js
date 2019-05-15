21f84311e3c9b337bdf4d16e2231f76b12cd02c2
mkoijn6 <puneet@cosmiclabs.co>
2019-05-15 21:16:26 +0530

    modal done forms.

---
+++ public/javascripts/getForm.js
@@ -0,0 +1,18 @@
+function getForm(route, modalContent = '#commonModalContent', modal = '#commonModal') {
+    $.ajax({
+        url: route.url,
+        type: route.type,
+        async: true,
+        statusCode: {
+            200: function (data) {
+                $(modal).fadeIn(200);
+                $(modalContent).html(data);
+            },
+            500: function (data) {
+                const newDocument = document.open("text/html", "replace");
+                newDocument.write(data);
+                newDocument.close();
+            }
+        }
+    });
+}
---
+++ public/javascripts/validateForm.js
@@ -0,0 +1,5 @@
+function validateForm(event, source) {
+    if (event.keyCode === 13) {
+        submitForm(source);
+    }
+}
