f1805fa7b9d145a2f2ecd2b53d0a3cd496f69e6f
Abhinav Kumar <abhinav@cosmiclabs.co>
2019-05-22 19:46:43 +0530

    Notification Optimization

--- public/javascripts/pushNotification.js
+++ public/javascripts/pushNotification.js
@@ -31,15 +31,4 @@ $(document).ready(function () {
         .catch(function (err) {
             console.log("Unable to get permission to notify.", err);
         });
-    messaging.onMessage(function (payload) {
-        if ($("#" + "notificationWindow").length !== 0) {
-            let newNotification = document.createElement('div');
-            newNotification.innerHTML = '</br>' + " (New) " + JSON.parse(JSON.stringify(payload)).notification.title + " + " + JSON.parse(JSON.stringify(payload)).notification.body + '</br>';
-            newNotification.style.backgroundColor = "#09c866";
-            newNotification.setAttribute("onclick", "location.reload()");
-            document.getElementById("notificationWindow").insertBefore(newNotification, document.getElementById("notificationWindow").firstElementChild);
-            $('#notificationWindow').children().last().remove();
-            document.getElementById("notificationCounter").textContent = "New"
-        }
-    });
 });
