316beb475b6bd8f24f2e08f01a4a1cdd817fb58c
mkoijn6 <puneet@cosmiclabs.co>
2019-06-28 16:53:23 +0530

    smalll fix

--- app/controllers/results/WithToken.scala
+++ app/controllers/results/WithToken.scala
@@ -7,5 +7,5 @@ import play.api.http.Writeable
 import play.api.mvc.{RequestHeader, Result, Results}
 
 class WithToken @Inject()(accountTokens: AccountTokens) {
-  def Ok[C](content: C)(implicit request: RequestHeader, writeable: Writeable[C]): Result = Results.Ok(content).withSession(request.session - Security.TOKEN + (Security.USERNAME -> accountTokens.Service.refreshSessionToken(request.session.get(Security.USERNAME).getOrElse("ss"))))
+  def Ok[C](content: C)(implicit request: RequestHeader, writeable: Writeable[C]): Result = Results.Ok(content).withSession(request.session - Security.TOKEN + (Security.USERNAME -> accountTokens.Service.refreshSessionToken(request.session.get(Security.USERNAME).getOrElse(""))))
 }
