f5c654d1900442a6ca3a13eeeaedc96490b6702c
mkoijn6 <puneet@cosmiclabs.co>
2019-07-08 22:26:07 +0530

    zone login views

--- app/constants/Security.scala
+++ app/constants/Security.scala
@@ -2,7 +2,6 @@ package constants
 
 object Security {
   val USERNAME = "USERNAME"
-  val USER_TYPE = "USER_TYPE"
   val TOKEN = "TOKEN"
   val TOKEN_TIME = "TOKEN_TIME"
   val NOT_LOGGED_IN = "NOT_LOGGED_IN"
