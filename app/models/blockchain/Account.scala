2319ad67d189a8363cfa7e4cc34bcbe02afb70c7
Abhinav Kumar <abhinav@cosmiclabs.co>
2019-07-31 16:06:58 +0530

    changed name comdex to commitCentral. Corrected accountFileUpdate minor issue

--- app/models/blockchain/Account.scala
+++ app/models/blockchain/Account.scala
@@ -133,9 +133,7 @@ class Accounts @Inject()(protected val databaseConfigProvider: DatabaseConfigPro
   object Service {
 
     def create(username: String, password: String)(implicit executionContext: ExecutionContext): String = {
-      val request = addKey.Request(username, password, getSeed.Service.get().body)
-      val addKeyResponse = addKey.Service.post(request)
-      logger.info(request.toString)
+      val addKeyResponse = addKey.Service.post(addKey.Request(username, password, getSeed.Service.get().body))
       Await.result(add(Account(addKeyResponse.address, 0, addKeyResponse.pub_key, -1, 0, dirtyBit = false)), Duration.Inf)
     }
 
