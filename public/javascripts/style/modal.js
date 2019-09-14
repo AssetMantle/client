f3a76357dea89e3d3921fa3e812d696e19aa66d6
blackpanther8132 <blackpanther8132@gmail.com>
2019-09-14 14:22:23 +0530

    changed responsiveness of home page cards

--- public/javascripts/style/modal.js
+++ public/javascripts/style/modal.js
@@ -6,15 +6,3 @@ $(document).mouseup(function (e) {
         }
     })
 });
-
-$(document).ready(function(){
-    $("#NavigationBarMenuButton").click(function(){
-        $("#navigationBarLinks").slideToggle("slow");
-    });
-    if ($(window).width() < 500) {
-        $('.cmuk-navbar-container').addClass('mobileNavigationBar').removeClass('cmuk-navbar-container');
-        $('.cmuk-navbar').removeClass('cmuk-navbar');
-        $('.cmuk-navbar-right').removeClass('cmuk-navbar-right');
-        $('.cmuk-navbar-nav').removeClass('cmuk-navbar-nav');
-    }
-});
