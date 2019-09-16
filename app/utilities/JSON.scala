f424065c850fed41063620dfccb67ab2798efbb3
Abhinav Kumar <abhinav@cosmiclabs.co>
2019-09-16 19:02:44 +0530

    Merge branch 'comdexClient' of https://gitlab.com/deepanshutr/commitCentral into traderOnboarding
    # Conflicts:
    #	app/controllers/SetACLController.scala
    #	app/utilities/Email.scala
    #	app/utilities/FileResourceManager.scala
    #	app/views/component/master/issueAsset.scala.html
    #	app/views/component/master/organizationViewVerificationTraderKycDouments.scala.html
    #	app/views/component/master/reviewOrganizationCompletion.scala.html
    #	app/views/component/master/userUploadOrUpdateOrganizationKYC.scala.html
    #	app/views/component/master/viewPendingVerifyOrgnizationRequests.scala.html
    #	conf/routes

--- app/utilities/JSON.scala
+++ app/utilities/JSON.scala
@@ -37,14 +37,14 @@ object JSON {
     try {
       Json.fromJson[T](Json.parse(jsonString)) match {
         case JsSuccess(value: T, _: JsPath) => value
-        case errors: JsError => logger.info(errors.toString)
+        case errors: JsError => logger.error(errors.toString)
           throw new BaseException(constants.Response.JSON_PARSE_EXCEPTION)
       }
     }
     catch {
-      case jsonParseException: JsonParseException => logger.info(jsonParseException.getMessage, jsonParseException)
+      case jsonParseException: JsonParseException => logger.error(jsonParseException.getMessage, jsonParseException)
         throw new BaseException(constants.Response.JSON_PARSE_EXCEPTION)
-      case jsonMappingException: JsonMappingException => logger.info(jsonMappingException.getMessage, jsonMappingException)
+      case jsonMappingException: JsonMappingException => logger.error(jsonMappingException.getMessage, jsonMappingException)
         throw new BaseException(constants.Response.JSON_MAPPING_EXCEPTION)
     }
   }
