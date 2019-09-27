f6fd90651d21c2c8fe310965ff9da1caeee02154
Abhinav Kumar <abhinav@cosmiclabs.co>
2019-09-27 18:49:12 +0530

    removed unnecessary notification related js files

--- public/javascripts/notification.js
+++ public/javascripts/notification.js
@@ -13,3 +13,34 @@ $('#notificationBadge').ready(function () {
         }
     });
 });
+
+function markNotificationRead(target, accountID) {
+    let route = jsRoutes.controllers.NotificationController.markNotificationRead(accountID);
+    $.ajax({
+        url: route.url,
+        type: route.type,
+        async: true,
+        statusCode: {
+            200: function (data) {
+                $(target).addClass("read");
+                $('#notificationBadge').html(data);
+            }
+        }
+    });
+}
+
+function loadMoreNotifications() {
+    const route = jsRoutes.controllers.NotificationController.notificationPage(($(".notificationContainer").length));
+    $.ajax({
+        url: route.url,
+        type: route.type,
+        async: true,
+        statusCode: {
+            200: function (data) {
+                const loadMore = $(".notificationContainer .notification:last");
+                loadMore.before(data);
+                loadMore.remove();
+            }
+        }
+    });
+}
