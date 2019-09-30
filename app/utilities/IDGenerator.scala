d9cc12744547962cc50bdec5e056124258e9d657
Abhinav Kumar <abhinav@cosmiclabs.co>
2019-09-30 21:31:26 +0530

    random string correction

--- app/utilities/IDGenerator.scala
+++ app/utilities/IDGenerator.scala
@@ -8,24 +8,14 @@ object IDGenerator {
 
   val count = new AtomicInteger()
 
-  private val alphanumeric = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
-
-  private def getRandomAlphanumeric(length: Int): String = {
-    val sb = new StringBuilder
-    for (i <- 1 to length) {
-      sb.append(alphanumeric(Random.nextInt(alphanumeric.length)))
-    }
-    sb.toString
-  }
-
   def requestID()(implicit module: String): String = {
     val initials = module.split("_").foldLeft("")(_ + _.take(1))
-    Seq("RQ", initials, (count.getAndIncrement() % 89999 + 10000).toString, (System.currentTimeMillis() % 89999999 + 10000000).toString, getRandomAlphanumeric(32 - initials.length - 15)).mkString("")
+    Seq("RQ", initials, (count.getAndIncrement() % 89999 + 10000).toString, (System.currentTimeMillis() % 89999999 + 10000000).toString, Random.alphanumeric.take(32 - initials.length - 15).mkString).mkString("")
   }
 
   def ticketID()(implicit module: String): String = {
     val initials = module.split("_").foldLeft("")(_ + _.take(1))
-    Seq("TX", initials, (count.getAndIncrement() % 89999 + 10000).toString, (System.currentTimeMillis() % 89999999 + 10000000).toString, getRandomAlphanumeric(32 - initials.length - 15)).mkString("")
+    Seq("TX", initials, (count.getAndIncrement() % 89999 + 10000).toString, (System.currentTimeMillis() % 89999999 + 10000000).toString, Random.alphanumeric.take(32 - initials.length - 15).mkString).mkString("")
   }
 
   def hexadecimal: String = (-Math.abs(Random.nextLong)).toHexString.toUpperCase
