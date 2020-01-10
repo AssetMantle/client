d0dc62d99b8eebcce1c1e31d58fec5007397c9ee
prashantkrv <prashantkrv96@gmail.com>
2020-01-10 15:21:53 +0530

    removed unnecessary  code

--- gatling/simulation/feeders/OrganizationLoginFeeder.scala
+++ gatling/simulation/feeders/OrganizationLoginFeeder.scala
@@ -10,7 +10,6 @@ object OrganizationLoginFeeder {
 
   def arrayConstructor(users: Int): Array[Map[String, String]] = {
     val feed = new Array[Map[String, String]](users)
-    //for (id <- 0 until users) feed(id) = Map(Test.TEST_ORGANIZATION_USERNAME -> feeders.UsernameFeeder.usernameFeed(id)(Test.TEST_USERNAME), Test.TEST_ORGANIZATION_PASSWORD -> feeders.UsernameFeeder.usernameFeed(id)(Test.TEST_USERNAME), Test.ID -> id.toString)
     for (id <- 0 until users){
       val username = "ORG1" + id.toString + Random.alphanumeric.take(8).mkString
       feed(id) = Map(Test.TEST_ORGANIZATION_USERNAME -> username, Test.TEST_ORGANIZATION_PASSWORD -> username)
