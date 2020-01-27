a4ddb07537a03367eabe73df9cdc4e3a3ac4d539
puneet2019 <puneet@cosmiclabs.co>
2020-01-27 13:49:48 +0530

    Merge branch 'wu' of https://gitlab.com/deepanshutr/commitCentral into comdexClient
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/controllers/WesternUnionController.scala
+++ app/controllers/WesternUnionController.scala
@@ -27,27 +27,29 @@ class WesternUnionController @Inject()(messagesControllerComponents: MessagesCon
   def westernUnion: Action[NodeSeq] = Action.async(parse.xml) {
     request =>
 
-        val requestBody = views.companion.master.WesternUnion.Request.fromXml(request.body)
-        val hash = secretKey  + requestBody.id.trim + requestBody.reference.trim + requestBody.externalReference.trim + requestBody.invoiceNumber.trim +
-          requestBody.buyerBusinessId.trim + requestBody.buyerFirstName.trim + requestBody.buyerLastName.trim + requestBody.createdDate.trim + requestBody.lastUpdatedDate.trim +
-          requestBody.status.trim + requestBody.dealType.trim + requestBody.paymentTypeId.trim + requestBody.paidOutAmount.trim
+      val requestBody = views.companion.master.WesternUnion.Request.fromXml(request.body)
+      val hash = secretKey + requestBody.id.trim + requestBody.reference.trim + requestBody.externalReference.trim + requestBody.invoiceNumber.trim +
+        requestBody.buyerBusinessId.trim + requestBody.buyerFirstName.trim + requestBody.buyerLastName.trim + requestBody.createdDate.trim + requestBody.lastUpdatedDate.trim +
+        requestBody.status.trim + requestBody.dealType.trim + requestBody.paymentTypeId.trim + requestBody.paidOutAmount.trim
 
-        (if (requestBody.requestSignature.trim == utilities.String.sha256Hash(hash)) {
-          val create=masterTransactionWURTCBRequests.Service.create((request.body \\ "request").mkString.replaceAll("[\\s\\n]+", ""))
-          for{
-            _<-create
-          }yield Ok(<response>
-            <code>200</code> <status>Success</status> <message>Transaction update successful.</message>
-          </response>).as("application/xml")
-        } else {
-          Future{Forbidden(<response>
+      (if (requestBody.requestSignature.trim == utilities.String.sha256Hash(hash)) {
+        val create = masterTransactionWURTCBRequests.Service.create((request.body \\ "request").mkString.replaceAll("[\\s\\n]+", ""))
+        for {
+          _ <- create
+        } yield Ok(<response>
+          <code>200</code> <status>Success</status> <message>Transaction update successful.</message>
+        </response>).as("application/xml")
+      } else {
+        Future {
+          Forbidden(<response>
             <code>403</code> <status>FORBIDDEN</status> <message>Comdex validation failure – invalid request signature</message>
           </response>).as("application/xml")
-        }}
-          ).recover{
-          case _: Exception => InternalServerError(<response>
-            <code>500</code> <status>INTERNAL_SERVER_ERROR</status> <message>Comdex validation failure</message>
-          </response>).as("application/xml")
         }
-        }
+      }
+        ).recover {
+        case _: Exception => InternalServerError(<response>
+          <code>500</code> <status>INTERNAL_SERVER_ERROR</status> <message>Comdex validation failure</message>
+        </response>).as("application/xml")
+      }
+  }
 }
