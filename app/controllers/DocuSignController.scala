0b40ba2b24ab380edfd5325431857e08d40b6cd9
prashantkrv <prashantkrv96@gmail.com>
2020-03-24 22:13:44 +0530

    fixes

--- app/controllers/DocuSignController.scala
+++ app/controllers/DocuSignController.scala
@@ -34,19 +34,23 @@ class DocuSignController @Inject()(messagesControllerComponents: MessagesControl
 
   def docuSignTest=Action{
 
-   // val accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU4NDU0MDExOCwiZXhwIjoxNTg0NTY4OTE4LCJVc2VySWQiOiJmNTczOWZhMi1kZGNkLTQwNmEtYTU5NS1lZDlhODk4NjJmMjEiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiZjU3MzlmYTItZGRjZC00MDZhLWE1OTUtZWQ5YTg5ODYyZjIxIiwiYW1yIjpbImludGVyYWN0aXZlIl0sImF1dGhfdGltZSI6MTU4NDU0MDExNCwicHdpZCI6ImUwNjJkNzc2LTJlMTgtNGU3ZC1iNDlkLWYyNDQwMGVkNThmYSJ9.qz3dosVS018kUmE6ti1uzRYG7BXXgls-J6e_QzLmzOVNe2EwNm1fQvqzM1TkLdkVItTb5GdzeWLhsL041lXIvUXl5YCi77mLZZjbU2AfTccB3PrA_du3JhTLW-YE4OmHkKXtB5uZta55Ty637duqJw4Q7_nsSMTX-WREIh_NF9MeZ8cI9NeK6OCmUYl3Rp5eqShrTGyAep2z8RFaaEwwAIqg0vQ9XPc1JDmG4-6sIF71ntQ2Ri2k2rTs1trVqRn7kAyiUTh8WZtidZoB0flH1dxgxPkhuc1Y7IM1h_KaSctMOgUpx9UbPJiCpoZRg5gZ3BTNckI8bLjFOeJTLjRhuA"
-    val accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU4NDc5MjQ1MywiZXhwIjoxNTg0ODIxMjUzLCJVc2VySWQiOiJmNTczOWZhMi1kZGNkLTQwNmEtYTU5NS1lZDlhODk4NjJmMjEiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiZjU3MzlmYTItZGRjZC00MDZhLWE1OTUtZWQ5YTg5ODYyZjIxIiwiYXV0aF90aW1lIjoxNTg0NzkyMjY3LCJwd2lkIjoiZTA2MmQ3NzYtMmUxOC00ZTdkLWI0OWQtZjI0NDAwZWQ1OGZhIn0.OLwyntGin9OV_fPccJe4JdVikWZW_dg9CGbER7vhJNfLB3l8CndFdbRNkO516fgNgAxElWQGu8HqK-0dJcOC3Rw4WESMAvLBAVRXaT_d4-21ZTv1gSc10xX_huFpU1KX_edxtnLHtndYf69M4-MDy0W8yqsmolvwCPhRZTPa3bSzDLs_RpbfRHPhhhL6NxKNgRhtLeDmeC85fvH-UmTkxok3BXlFwh7mwjLsY8hYqVP3Z4yDujOVio2-c4y5DNXoIwfMD3ftLkZPqBDrzr6woWq06lTqFrFn3RvndTnoVyDNFN7GYsawghsgVvt0X60D0dsgg_0pMTtYr4YRWlXb5A"
+    val accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU4NTA1MDg4MCwiZXhwIjoxNTg1MDc5NjgwLCJVc2VySWQiOiJmNTczOWZhMi1kZGNkLTQwNmEtYTU5NS1lZDlhODk4NjJmMjEiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiZjU3MzlmYTItZGRjZC00MDZhLWE1OTUtZWQ5YTg5ODYyZjIxIiwiYW1yIjpbImludGVyYWN0aXZlIl0sImF1dGhfdGltZSI6MTU4NTA1MDg3NiwicHdpZCI6ImUwNjJkNzc2LTJlMTgtNGU3ZC1iNDlkLWYyNDQwMGVkNThmYSJ9.15hYMK9r_x0S0NQfZmIAy5OPFvlKRbqQVWZdqJf6qkdQhI8nxDESp9m4WIre5u5PaCz8Rx0KUgZhRRyDf5YezFMxyHp7bjOJjclmeUOQGsWMdnXlRyZ3uodJTLJiQwHY26ZJK7aU2ShkZCxvq_afk_FZr5dDQ9V2VJOZNJaFxo_K3gt_6q3OkkLvbkx6CrqCFi4psG7rcdx62OpwGuzSf0s2X1bZHHWUSgjHTOWv7QmVVOrFCEactICw_y7mEJlGEw2dwP2l-zuuiG6Ae7Ao5ES5jHQ9R_srX_J9I7L6ykYPf9yNZO12UApRplWCWxkgbbwMXXIhpRJrc_cKL4TMVQ"
     val accountID="10156924"
-    val signerName="Prashant"
+    val signerName="Prashant Kumar Varun"
     val signerEmail="prashantkrv96@gmail.com"
     val baseUrl="http://localhost:9000"
     val clientUserID="123"
     val authenticationMethod="None"
     val basePath="https://demo.docusign.net/restapi"
+    val tokenExpiration= (60.toLong).*(480:Long)
 
-   // val base64File=Base64.encodeBase64String(utilities.FileOperations.convertToByteArray(utilities.FileOperations.newFile("/home/persistence-1/projects/commitCentral/public/images/", "44678.pdf")))
+    // val base64File=Base64.encodeBase64String(utilities.FileOperations.convertToByteArray(utilities.FileOperations.newFile("/home/persistence-1/projects/commitCentral/public/images/", "44678.pdf")))
     val fileByteArray=utilities.FileOperations.convertToByteArray(utilities.FileOperations.newFile("/home/persistence-1/projects/commitCentral/public/images/", "44678.pdf"))
-    val base64File2= Base64.encode(fileByteArray).toString
+    println("fileByteArray"+fileByteArray)
+    val base64File= Base64.encode(fileByteArray).toString
+    val base64File2=new String(Base64.encode(fileByteArray))
+    println(base64File2)
+    println("base64file"+base64File)
     val document= new Document()
     document.setDocumentBase64(base64File2)
     document.setName("Example document")
@@ -64,8 +68,8 @@ class DocuSignController @Inject()(messagesControllerComponents: MessagesControl
     signHere.setPageNumber("1")
     signHere.setRecipientId("1")
     signHere.setTabLabel("SignHereTab")
-    signHere.setXPosition("195")
-    signHere.setYPosition("147")
+    signHere.setXPosition("100")
+    signHere.setYPosition("100")
 
     val signerTabs=new Tabs()
     signerTabs.setSignHereTabs(Arrays.asList(signHere))
@@ -83,10 +87,9 @@ class DocuSignController @Inject()(messagesControllerComponents: MessagesControl
 
 
     val apiClient= new ApiClient(basePath)
+    apiClient.setAccessToken(accessToken,tokenExpiration)
+    //apiClient.addDefaultHeader("Authorization", "Bearer " + accessToken)
 
-    apiClient.addDefaultHeader("Authorization", "Bearer " + accessToken)
-   println(apiClient)
-   println(envelopeDefinition)
     val envelopesApi=new EnvelopesApi(apiClient)
     val results=envelopesApi.createEnvelope(accountID, envelopeDefinition)
     val envelopeId = results.getEnvelopeId()
@@ -100,7 +103,7 @@ class DocuSignController @Inject()(messagesControllerComponents: MessagesControl
 
     val results1 = envelopesApi.createRecipientView(accountID, envelopeId, viewRequest)
     val redirectUrl = results1.getUrl
-
+    println(redirectUrl)
     val redirect= Redirect(redirectUrl)
 
     redirect
