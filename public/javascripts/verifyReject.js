d189edde548694c6e4c0675b82742c7ef59afb9b
mkoijn6 <puneet@cosmiclabs.co>
2020-03-27 17:44:31 +0530

    Merge branch 'salesQuoteInvite' of https://gitlab.com/deepanshutr/commitCentral into salesQuoteInvite
    # Conflicts:
    #	app/constants/Test.scala
    #	app/controllers/DocuSignController.scala
    #	build.sbt
    #	conf/routes

--- public/javascripts/verifyReject.js
+++ public/javascripts/verifyReject.js
@@ -2,7 +2,3 @@ function verifyReject(source, target, statusFieldID, status) {
     $('#' + statusFieldID).prop('checked', status);
     submitForm(source, target);
 }
-
-function hideElement(id){
-    $("#" + id).hide();
-}
