c1be8f986214695f631854bff75cf30d9120103b
mkoijn6 <puneet@cosmiclabs.co>
2020-03-31 14:30:26 +0530

    Merge branch 'docuSignPOC' of https://gitlab.com/deepanshutr/commitCentral into docuSignPOC
    # Conflicts:
    #	app/controllers/DocuSignController.scala

--- public/javascripts/verifyReject.js
+++ public/javascripts/verifyReject.js
@@ -1,4 +1,4 @@
-function verifyReject(source, target, statusFieldID, status) {
-    $('#' + statusFieldID).prop('checked', status);
+function verifyReject(source, target, status) {
+    $(source).closest("form").find('input[type=checkbox]').prop('checked', status);
     submitForm(source, target);
 }
