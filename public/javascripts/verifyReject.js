a299ad9f78919075372a1e2b3b13d200dcc9c86d
puneet2019 <puneet@cosmiclabs.co>
2020-04-15 11:39:13 +0530

    added zone trades tab

--- public/javascripts/verifyReject.js
+++ public/javascripts/verifyReject.js
@@ -5,6 +5,5 @@ function verifyReject(source, target, unusedInput, status) {
 
 function verifyRejectCommonForm(source, target, status) {
     $(source).closest("form").find('[type=checkbox]').prop('checked', status);
-    console.log(target, status)
     submitForm(source, target);
 }
