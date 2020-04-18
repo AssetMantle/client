2cbbf496b674bca383caa2927e701cef5ad10a89
blackpanther8132 <raju@persistence.one>
2020-04-18 18:25:24 +0530

    updated sticky position js

--- public/javascripts/style/stickyCard.js
+++ public/javascripts/style/stickyCard.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
-    $(window).resize(function() {
-        if ($(window).width() > 1200) {
-            $(".sticky").css({
-                "position": "sticky",
-                "top": 71,
-            })
-        } else {
-            $(".sticky").css({
-                "position": "static",
-                "top": 0,
-            })
-        }
-    });
+    if ($(window).width() > 1200) {
+        $(".sticky").css({
+            "position": "sticky",
+            "top": 71,
+        })
+    } else {
+        $(".sticky").css({
+            "position": "static",
+            "top": 0,
+        })
+    }
 });
