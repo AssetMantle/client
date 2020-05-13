1d2571971b52c490a0e62d9e4737c87646cd3e3b
avkr003 <abhinav@persistence.one>
2020-05-13 20:20:31 +0530

    Merge branch 'comdexClient' of https://gitlab.com/deepanshutr/commitCentral into hideGas
    # Conflicts:
    #	app/views/component/master/zoneVerifyTrader.scala.html

---
+++ app/views/companion/master/AddUBO.scala
@@ -0,0 +1,20 @@
+package views.companion.master
+
+import play.api.data.Form
+import play.api.data.Forms.{mapping, optional, seq}
+
+object AddUBO {
+
+  val form = Form(
+    mapping(
+      constants.FormField.PERSON_FIRST_NAME.name -> constants.FormField.PERSON_FIRST_NAME.field,
+      constants.FormField.PERSON_LAST_NAME.name -> constants.FormField.PERSON_LAST_NAME.field,
+      constants.FormField.SHARE_PERCENTAGE.name -> constants.FormField.SHARE_PERCENTAGE.field,
+      constants.FormField.RELATIONSHIP.name -> constants.FormField.RELATIONSHIP.field,
+      constants.FormField.TITLE.name -> constants.FormField.TITLE.field
+    )(Data.apply)(Data.unapply)
+  )
+
+  case class Data(personFirstName: String, personLastName: String, sharePercentage: Double, relationship: String, title: String)
+
+}
