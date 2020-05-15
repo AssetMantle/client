91beb3b199e8aa7aa96d0e05ce040e1ef21ea09a
avkr003 <abhinav@persistence.one>
2020-05-15 16:15:04 +0530

    Self Review

--- public/javascripts/componentResource.js
+++ public/javascripts/componentResource.js
@@ -1,14 +1,12 @@
 function componentResource(source, route, loadingSpinnerID = 'commonSpinner', event = '') {
     const div = $('#' + source);
     let loadingSpinner = $('#' + loadingSpinnerID);
-    console.log(loadingSpinnerID);
     $.ajax({
         url: route.url,
         type: route.type,
         async: true,
         global: showSpinner(event),
         beforeSend: function () {
-            console.log(loadingSpinnerID);
             loadingSpinner.show();
         },
         complete: function () {
