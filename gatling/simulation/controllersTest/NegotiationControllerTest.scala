e07a95e0941a225a0a975e6dfbea04e1068530a2
prashant <prashantkrv96@gmail.com>
2020-05-22 19:28:44 +0530

    gatling changes

--- gatling/simulation/controllersTest/NegotiationControllerTest.scala
+++ gatling/simulation/controllersTest/NegotiationControllerTest.scala
@@ -18,6 +18,8 @@ class NegotiationControllerTest extends Simulation {
 
 object negotiationControllerTest {
 
+  val negotiationTermList = Seq("ASSET_DESCRIPTION", "PRICE", "QUANTITY", "ASSET_OTHER_DETAILS", "PAYMENT_TERMS", "DOCUMENT_LIST")
+
   val negotiationRequestScenario: ScenarioBuilder = scenario("NegotiationRequest")
     .exec(http("IssueAssetDetailForm_GET")
       .get(routes.NegotiationController.requestForm().url)
@@ -75,7 +77,7 @@ object negotiationControllerTest {
 
   val acceptNegotiationRequest: ScenarioBuilder = scenario("AcceptNegotiationRequest")
     .exec(http("AcceptNegotiationRequestForm_GET")
-      .get(session=>routes.NegotiationController.acceptRequestForm(session(Test.TEST_NEGOTIATION_ID).as[String]).url)
+      .get(session => routes.NegotiationController.acceptRequestForm(session(Test.TEST_NEGOTIATION_ID).as[String]).url)
       .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN)))
     .pause(2)
     .exec(http("AcceptNegotiationRequest_POST")
@@ -84,14 +86,14 @@ object negotiationControllerTest {
         constants.FormField.ID.name -> "${%s}".format(Test.TEST_NEGOTIATION_ID),
         constants.FormField.GAS.name -> "${%s}".format(Test.TEST_GAS),
         constants.FormField.PASSWORD.name -> "${%s}".format(Test.TEST_PASSWORD),
-        constants.FormField.ID.name -> "${%s}".format(Test.CSRF_TOKEN)))
+        Test.CSRF_TOKEN -> "${%s}".format(Test.CSRF_TOKEN)))
       .check(substring("Document List").exists)
       .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN))
     )
 
   val rejectNegotiationRequest: ScenarioBuilder = scenario("RejectNegotiationRequest")
     .exec(http("RejectNegotiationRequestForm_GET")
-      .get(session=>routes.NegotiationController.rejectRequestForm(session(Test.TEST_NEGOTIATION_ID).as[String]).url)
+      .get(session => routes.NegotiationController.rejectRequestForm(session(Test.TEST_NEGOTIATION_ID).as[String]).url)
       .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN)))
     .pause(2)
     .exec(http("AcceptNegotiationRequest_POST")
@@ -99,15 +101,80 @@ object negotiationControllerTest {
       .formParamMap(Map(
         constants.FormField.ID.name -> "${%s}".format(Test.TEST_NEGOTIATION_ID),
         constants.FormField.COMMENT.name -> "",
-        constants.FormField.ID.name -> "${%s}".format(Test.CSRF_TOKEN)))
+        Test.CSRF_TOKEN -> "${%s}".format(Test.CSRF_TOKEN)))
       .check(substring("Document List").exists)
       .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN))
     )
 
   val acceptNegotiationTerms: ScenarioBuilder = scenario("AcceptNegotiationTerms")
-    .exec(http("Accept")
-        .get(routes.NegotiationController.acceptOrRejectNegotiationTermsForm())
+    .foreach(negotiationTermList, constants.Test.TERM_TYPE) {
+      exec(http("AcceptOrRejectNegotiationTermForm_GET")
+        .get(session => routes.NegotiationController.acceptOrRejectNegotiationTermsForm(session(Test.TEST_NEGOTIATION_ID).as[String], session(constants.Test.TERM_TYPE).as[String]).url)
+        .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN))
+      )
+        .pause(2)
+        .exec(http("AcceptNegotiationTerm_POST")
+          .post(routes.NegotiationController.acceptOrRejectNegotiationTerms().url)
+          .formParamMap(Map(
+            constants.FormField.ID.name -> "${%s}".format(Test.TEST_NEGOTIATION_ID),
+            constants.FormField.TERM_TYPE.name -> "${%s}".format(Test.TERM_TYPE),
+            constants.FormField.STATUS.name -> true,
+            Test.CSRF_TOKEN -> "${%s}".format(Test.CSRF_TOKEN)))
+        )
+    }
+
+  val confirmAllNegotiationTerms: ScenarioBuilder = scenario("ConfirmAllNegotiationTerms")
+    .exec(http("ConfirmAllNegotiationTermsForm_GET")
+      .get(session => routes.NegotiationController.confirmAllNegotiationTermsForm(session(Test.TEST_NEGOTIATION_ID).as[String]).url)
+      .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN))
     )
+    .pause(2)
+    .exec(http("ConfirmAllNegotiationTerms_POST")
+      .post(routes.NegotiationController.confirmAllNegotiationTerms().url)
+      .formParamMap(Map(
+        constants.FormField.ID.name -> "${%s}".format(Test.TEST_NEGOTIATION_ID),
+        Test.CSRF_TOKEN -> "${%s}".format(Test.CSRF_TOKEN)))
+    )
+
+  val uploadContract: ScenarioBuilder = scenario("UploadContract")
+    .feed(ImageFeeder.imageFeed)
+    .exec(http("UploadContractForm" + "_GET")
+      .get(session => routes.FileController.uploadNegotiationForm(session(Test.TEST_NEGOTIATION_ID).as[String],constants.File.Negotiation.CONTRACT).url)
+      .check(substring("BROWSE").exists)
+      .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN))
+    )
+    .pause(2)
+    .exec(http("UploadContract" + "_POST")
+      .post(session => routes.FileController.uploadNegotiation(constants.File.Negotiation.CONTRACT).url)
+      .formParamMap(Map(
+        Test.CSRF_TOKEN -> "${%s}".format(Test.CSRF_TOKEN),
+        Form.RESUMABLE_CHUNK_NUMBER -> "1",
+        Form.RESUMABLE_CHUNK_SIZE -> "1048576",
+        Form.RESUMABLE_TOTAL_SIZE -> "${%s}".format(Test.TEST_FILE_SIZE),
+        Form.RESUMABLE_IDENTIFIER -> "document",
+        Form.RESUMABLE_FILE_NAME -> "${%s}".format(Test.TEST_FILE_NAME)))
+      .bodyPart(RawFileBodyPart("file", Test.IMAGE_FILE_FEED + "${%s}".format(Test.TEST_FILE_NAME))
+        .transferEncoding("binary")).asMultipartForm)
+    .exec(
+      http("Store_Contract")
+        .get(session => routes.FileController.storeNegotiation(session(Test.TEST_FILE_NAME).as[String], constants.File.Negotiation.CONTRACT,session(Test.TEST_NEGOTIATION_ID).as[String]).url)
+    )
+    .pause(2)
+
+  val updateContractSigned: ScenarioBuilder = scenario("UpdateContractSigned")
+    .exec(http("UpdateContractSignedForm_GET")
+      .get(session => routes.NegotiationController.updateContractSignedForm(session(Test.TEST_NEGOTIATION_ID).as[String]).url)
+      .check(css("[name=%s]".format(Test.CSRF_TOKEN), "value").saveAs(Test.CSRF_TOKEN))
+    )
+    .pause(2)
+    .exec(http("UpdateContractSigned_POST")
+      .post(routes.NegotiationController.updateContractSigned().url)
+      .formParamMap(Map(
+        constants.FormField.ID.name -> "${%s}".format(Test.TEST_NEGOTIATION_ID),
+        Test.CSRF_TOKEN -> "${%s}".format(Test.CSRF_TOKEN)))
+    )
+
+  val uploadAssetDocuments: ScenarioBuilder = scenario("UpdateContractSigned")
 
   val issueAssetRequestScenario: ScenarioBuilder = scenario("IssueAssetRequest")
     .feed(AssetTypeFeeder.assetTypeFeed)
