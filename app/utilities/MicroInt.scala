c0f0ead0c2d7535e7e4432f40b9f31fe5c63142f
prashant <prashantkrv96@gmail.com>
2020-06-25 17:18:29 +0530

    MicroInt.scala added

--- app/utilities/MicroInt.scala
+++ app/utilities/MicroInt.scala
@@ -1,15 +1,15 @@
 package utilities
 
-class MicroInt(val value:Long){
+class MicroInt(val value: Long) {
 
-  def this(a:String)= this((a.toDouble * 1000000).toLong)
+  def this(value: String) = this((value.toDouble * 1000000).toLong)
 
-  def this(b:Int)= this((b*1000000).toLong)
+  def this(value: Int) = this((value * 1000000).toLong)
 
-  def this(c:Double)=this((c * 1000000).toLong)
+  def this(value: Double) = this((value * 1000000).toLong)
 
-  def string=(value.toDouble/1000000).toString
+  def string = (value.toDouble / 1000000).toString
 
-  def double=value.toDouble/1000000
+  def double = value.toDouble / 1000000
 
 }
