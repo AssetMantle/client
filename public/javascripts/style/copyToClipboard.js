c00dc04444bfc3bfe2c98fcc03b0ad05b7cac40c
blackpanther8132 <blackpanther8132@gmail.com>
2020-07-22 12:33:35 +0530

    adding common coyp text and ui updates.

--- public/javascripts/style/copyToClipboard.js
+++ public/javascripts/style/copyToClipboard.js
@@ -1,8 +1,8 @@
-function copyToClipboard(elementId) {
+function copyToClipboard(elementId, id) {
     // Create an auxiliary hidden input
     let aux = document.createElement("input");
     // Get the text from the element passed into the input
-    aux.setAttribute("value", $('#' + elementId).html());
+    aux.setAttribute("value",  elementId);
     // Append the aux input to the body
     document.body.appendChild(aux);
     // Highlight the content
@@ -12,11 +12,11 @@ function copyToClipboard(elementId) {
     // Remove the input from the body
     document.body.removeChild(aux);
 
-    var tooltip = document.getElementById("toolTip");
+    var tooltip = document.getElementById(id);
     tooltip.innerHTML = "Copied";
 
 }
-function afterCopy() {
-    var tooltip = document.getElementById("toolTip");
-    tooltip.innerHTML = "Copy to clipboard";
+function afterCopy(id) {
+    var tooltip = document.getElementById(id);
+    tooltip.innerHTML = "Copy";
 }
