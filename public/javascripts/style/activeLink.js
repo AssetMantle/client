c39897cac9ba3e425c5f55636a1579ecf412cc48
blackpanther8132 <blackpanther8132@gmail.com>
2020-08-28 16:28:29 +0530

    adding active class when route null

--- public/javascripts/style/activeLink.js
+++ public/javascripts/style/activeLink.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     $('nav a').filter(function(idx, elem) {
+        console.log()
         if(elem.pathname !== null && elem.pathname === window.location.pathname){
             $(elem).addClass('active');
+        }else if(window.location.pathname == '/'){
+            $('#dashBoard a').addClass('active');
         }
     });
 });
