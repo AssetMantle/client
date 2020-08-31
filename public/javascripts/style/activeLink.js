6582163244d6e7e02ddda8ed244f188c8833dae4
blackpanther8132 <blackpanther8132@gmail.com>
2020-08-31 13:27:18 +0530

    removing console.log from activlink

--- public/javascripts/style/activeLink.js
+++ public/javascripts/style/activeLink.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $('nav a').filter(function(idx, elem) {
-        console.log()
         if(elem.pathname !== null && elem.pathname === window.location.pathname){
             $(elem).addClass('active');
         }else if(window.location.pathname == '/'){
