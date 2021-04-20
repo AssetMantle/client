23fa2ee864ccaa9c9008a718d594a6711f8ab4a7
Dheeraj Dubey <dheeraj@persistence.one>
2021-04-20 16:22:00 +0530

    Update RegularExpression.scala

--- app/constants/RegularExpression.scala
+++ app/constants/RegularExpression.scala
@@ -10,7 +10,7 @@ object RegularExpression {
   val ACCOUNT_ID: Regex = """^[a-zA-Z0-9!@#$%^&* ._-]*$""".r
   val MOBILE_NUMBER: Regex = """^(\+\d{1,3}[- ]?)?\d{6,14}$""".r
   val PEG_HASH: Regex = """^[0-9]*$""".r
-  val BILL_OF_LADING_NUMBER = """^[A-Za-z0-9/,.-]*$""".r
+  val BILL_OF_LADING_NUMBER = """^[^\s]+[A-Za-z0-9/ ,.-]*$""".r
   val NAME = """^[A-Za-z0-9/ ,.-]*$""".r
   val ADDRESS = """^[A-Za-z0-9/ ,.-]*$""".r
   val ALL_LETTERS: Regex = """^[a-zA-z]*$""".r
