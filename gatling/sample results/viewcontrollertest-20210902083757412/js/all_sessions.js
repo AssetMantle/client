b7e019cd029994c411f50526b017208dd8b02751
namankabra <namanmkabra@gmail.com>
2021-09-02 14:11:43 +0530

    Sample Results

---
+++ gatling/sample results/viewcontrollertest-20210902083757412/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1630571879000,4],[1630571880000,4],[1630571881000,4],[1630571882000,4],[1630571883000,4]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
