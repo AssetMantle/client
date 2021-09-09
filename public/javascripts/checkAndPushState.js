25105edcdc151e80e49572eb9e141190620b4ca4
ciberexplosion <sachinmatta@outlook.com>
2021-09-09 18:35:07 +0530

    resolved 400 bad request

--- public/javascripts/checkAndPushState.js
+++ public/javascripts/checkAndPushState.js
@@ -3,7 +3,12 @@ function checkAndPushState(route, parameter, functionName) {
         if (route === "" && parameter === "") {
             window.history.pushState(functionName, "persistence", "/");
         } else {
-            address = "/" + route.split('/')[1] + "/" + parameter.toString()
+            let address = "";
+            if (parameter !== "") {
+                address = "/" + route.split('/')[1] + "/" + parameter.toString()
+            } else {
+                address = "/" + route.split('/')[1]
+            }
             window.history.pushState(functionName, "persistence", address);
         }
     } else {
