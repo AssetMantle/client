459d87a9d89fbb19542223e703e14f90a37edd69
ciberexplosion <sachinmatta@outlook.com>
2021-09-13 13:19:51 +0530

    resolved navbar highlight not changing issue

--- public/javascripts/style/activeLink.js
+++ public/javascripts/style/activeLink.js
@@ -1,24 +1,63 @@
-$(document).ready(function () {
-    $('nav .cmuk-navbar-nav li a').filter(function(idx, elem) {
-        const location = window.location.pathname;
-        if('/'+elem.getAttribute("id") === location){
+let lastUrl = location.href;
+
+function navBar() {
+    $('nav .cmuk-navbar-nav li a').filter(function (idx, elem) {
+        if ('/' + elem.getAttribute("id") === window.location.pathname) {
             $(elem).addClass('active');
-        }else if(location == '/'){
-            $('#dashBoard').addClass('active');
-        }else if(location.startsWith('/validator')){
-            $('#validators').addClass('active');
-        }else if(location.startsWith('/block')){
-            $('#blocks').addClass('active');
-        }else if(location.startsWith('/transaction')){
-            $('#transactions').addClass('active');
+        } else {
+            switch (history.state) {
+                case "block":
+                case "blockList":
+                    $('#blocks').addClass('active');
+                    break;
+                case "validator":
+                case "validatorList":
+                    $('#validators').addClass('active');
+                    break;
+                case "transaction":
+                case "transactionList":
+                    $('#transactions').addClass('active');
+                    break;
+                case "":
+                    $('#dashBoard').addClass('active');
+                    break;
+                default:
+                    var elems = document.querySelectorAll(".active");
+                    [].forEach.call(elems, function (el) {
+                        el.classList.remove("active");
+                    });
+            }
         }
     });
-});
+};
 
-function setActiveLink(e){
+function setActiveLink(e) {
     var elems = document.querySelectorAll(".active");
-    [].forEach.call(elems, function(el) {
+    [].forEach.call(elems, function (el) {
         el.classList.remove("active");
     });
     e.target.className = "active";
 }
+
+$(document).ready(function () {
+    console.log("hey")
+    navBar()
+})
+
+new MutationObserver(() => {
+    const url = location.href;
+    if (url !== lastUrl) {
+        lastUrl = url;
+        onUrlChange();
+    }
+}).observe(document, {subtree: true, childList: true});
+
+function onUrlChange() {
+    var elems = document.querySelectorAll(".active");
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("active");
+    });
+    navBar()
+}
+
+
