a1e5649014e9b9d18653af2b802e1fa3b70cc720
bahdotsh <appu.yess@gmail.com>
2022-01-05 10:02:34 +0530

    token price updated from coingecko

---
+++ app/queries/responses/coingecko/TickerResponse.scala
@@ -0,0 +1,15 @@
+package queries.responses.coingecko
+
+import play.api.libs.json.{Json, Reads}
+import transactions.Abstract.BaseResponse
+
+object TickerResponse {
+  case class Data(usd: Double)
+
+  implicit val dataReads: Reads[Data] = Json.reads[Data]
+
+  case class Response(persistence: Data) extends BaseResponse
+
+  implicit val responseReads: Reads[Response] = Json.reads[Response]
+
+}
