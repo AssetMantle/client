ab88d725bf6b53c257412c2e80ba8b8971a2a07e
avkr003 <abhinav@persistence.one>
2022-01-25 15:26:34 +0530

    Putting new tables in 2.sql

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,53 @@
+# --- !Ups
+
+CREATE TABLE IF NOT EXISTS BLOCKCHAIN."Authorization_BC"
+(
+    "granter"              VARCHAR NOT NULL,
+    "grantee"              VARCHAR NOT NULL,
+    "msgTypeURL"           VARCHAR NOT NULL,
+    "grantedAuthorization" VARCHAR NOT NULL,
+    "expiration"           VARCHAR NOT NULL,
+    "createdBy"            VARCHAR,
+    "createdOn"            TIMESTAMP,
+    "createdOnTimeZone"    VARCHAR,
+    "updatedBy"            VARCHAR,
+    "updatedOn"            TIMESTAMP,
+    "updatedOnTimeZone"    VARCHAR,
+    PRIMARY KEY ("granter", "grantee", "msgTypeURL")
+);
+
+CREATE TABLE IF NOT EXISTS BLOCKCHAIN."FeeGrant_BC"
+(
+    "granter"           VARCHAR NOT NULL,
+    "grantee"           VARCHAR NOT NULL,
+    "allowance"         VARCHAR NOT NULL,
+    "createdBy"         VARCHAR,
+    "createdOn"         TIMESTAMP,
+    "createdOnTimeZone" VARCHAR,
+    "updatedBy"         VARCHAR,
+    "updatedOn"         TIMESTAMP,
+    "updatedOnTimeZone" VARCHAR,
+    PRIMARY KEY ("granter", "grantee")
+);
+
+ALTER TABLE BLOCKCHAIN."Transaction"
+    DROP COLUMN "status";
+
+CREATE TRIGGER FEE_GRANT_LOG
+    BEFORE INSERT OR UPDATE
+    ON BLOCKCHAIN."FeeGrant_BC"
+    FOR EACH ROW
+EXECUTE PROCEDURE PUBLIC.INSERT_OR_UPDATE_LOG();
+
+CREATE TRIGGER AUTHORIZATION_BC_LOG
+    BEFORE INSERT OR UPDATE
+    ON BLOCKCHAIN."Authorization_BC"
+    FOR EACH ROW
+EXECUTE PROCEDURE PUBLIC.INSERT_OR_UPDATE_LOG();
+
+# --- !Downs
+DROP TRIGGER IF EXISTS AUTHORIZATION_BC_LOG ON BLOCKCHAIN."Authorization_BC" CASCADE;
+DROP TRIGGER IF EXISTS FEE_GRANT_LOG ON BLOCKCHAIN."FeeGrant_BC" CASCADE;
+
+DROP TABLE IF EXISTS BLOCKCHAIN."Authorization_BC" CASCADE;
+DROP TABLE IF EXISTS BLOCKCHAIN."FeeGrant_BC" CASCADE;
