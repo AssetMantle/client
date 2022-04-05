e3e1c8f038c81d0ccf2126deeecf98d1c09b96f9
chudasamajd <chudasamajagdish12@gmail.com>
2022-04-05 23:47:22 +0530

    feat: change master to mantle theme

--- public/javascripts/pieChart.js
+++ public/javascripts/pieChart.js
@@ -2,9 +2,9 @@ function pieChart(chartID, keys, values, showLegend) {
     let nameList = keys.replace('Set(', '').replace(')', '').split(', ');
     let valueList = values.replace('MapLike.DefaultValuesIterable(', '').replace(')', '').split(', ');
     let totalValue = 0.0;
-    let colors = [];
+    let colors = [theme_colors.primary];
     let colorPrefixes = ["e", 4 , 1 , 2, 6, "b", 9, 3, "f", 5, 6, 0 , "e", 2, 8, "b", 9, 3, "f", 5, "a" , 7 , "c", 2, 6, "b", 9, 3, "f", 5,"e", 4 , 1 , 2, 6, "b", 9, 3, "f", 5, 6, 0 , "e", 2, 8, "b", 9, 3, "f", 5];
-    for (let i = 0; i < valueList.length; i++) {
+    for (let i = 1; i < valueList.length; i++) {
         totalValue = totalValue + parseFloat(valueList[i]);
         if(colorPrefixes.length <= i){
             colors.push('#' +colorPrefixes[i - colorPrefixes.length] + ((i + 50) * 884).toString());
---
+++ public/javascripts/theme.js
@@ -0,0 +1,3 @@
+const theme_colors = {
+    primary : "#FFC640"
+};
