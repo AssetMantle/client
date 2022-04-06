65b59086ce288e4342958839e43e9eb9a1a08c15
Abhinav Kumar <57705190+avkr003@users.noreply.github.com>
2022-04-06 20:11:52 +0530

    minor changes (#22)

--- conf/evolutions/default/0.sql
+++ conf/evolutions/default/0.sql
@@ -1,7 +1,7 @@
 -- Create database and user scripts. To be run before evolutions are started.
 
-CREATE USER "persistence" WITH PASSWORD 'persistence';
+CREATE USER "assetMantle" WITH PASSWORD 'assetMantle';
 
-CREATE DATABASE "persistence" WITH OWNER = "persistence";
+CREATE DATABASE "assetMantle" WITH OWNER = "assetMantle";
 
-ALTER USER "persistence" SET SEARCH_PATH = "$user", BLOCKCHAIN, BLOCKCHAIN_TRANSACTION, KEY_BASE, MASTER, MASTER_TRANSACTION, WESTERN_UNION, DOCUSIGN, MEMBER_CHECK;
+ALTER USER "assetMantle" SET SEARCH_PATH = "$user", BLOCKCHAIN, BLOCKCHAIN_TRANSACTION, KEY_BASE, MASTER, MASTER_TRANSACTION, WESTERN_UNION, DOCUSIGN, MEMBER_CHECK;
