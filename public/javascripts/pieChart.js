99251dd6c78c07c7837b671578fc2e3aa214d32d
avkr003 <abhinav@persistence.one>
2022-05-27 07:30:31 +0530

    upgrade

--- public/javascripts/pieChart.js
+++ public/javascripts/pieChart.js
@@ -3,13 +3,13 @@ function pieChart(chartID, keys, values, showLegend) {
     let valueList = values.replace('MapLike.DefaultValuesIterable(', '').replace(')', '').split(', ');
     let totalValue = 0.0;
     let colors = [theme_colors.primary];
-    let colorPrefixes = ["e", 4 , 1 , 2, 6, "b", 9, 3, "f", 5, 6, 0 , "e", 2, 8, "b", 9, 3, "f", 5, "a" , 7 , "c", 2, 6, "b", 9, 3, "f", 5,"e", 4 , 1 , 2, 6, "b", 9, 3, "f", 5, 6, 0 , "e", 2, 8, "b", 9, 3, "f", 5];
+    let colorPrefixes = ["e", 4, 1, 2, 6, "b", 9, 3, "f", 5, 6, 0, "e", 2, 8, "b", 9, 3, "f", 5, "a", 7, "c", 2, 6, "b", 9, 3, "f", 5, "e", 4, 1, 2, 6, "b", 9, 3, "f", 5, 6, 0, "e", 2, 8, "b", 9, 3, "f", 5];
     for (let i = 1; i < valueList.length; i++) {
         totalValue = totalValue + parseFloat(valueList[i]);
-        if(colorPrefixes.length <= i){
-            colors.push('#' +colorPrefixes[i - colorPrefixes.length] + ((i + 50) * 884).toString());
-        }else {
-            colors.push('#' +colorPrefixes[i] + ((i + 50) * 884).toString());
+        if (colorPrefixes.length <= i) {
+            colors.push('#' + colorPrefixes[i - colorPrefixes.length] + ((i + 50) * 884).toString());
+        } else {
+            colors.push('#' + colorPrefixes[i] + ((i + 50) * 884).toString());
         }
     }
 
@@ -35,7 +35,7 @@ function pieChart(chartID, keys, values, showLegend) {
                         let name = nameList[tooltipItem.index];
                         let value = valueList[tooltipItem.index];
                         let dataset = data.datasets[tooltipItem.datasetIndex];
-                        return [name, (value * 100.0 / totalValue).toFixed(2) + "%", value];
+                        return [name, (value * 100.0 / totalValue).toFixed(2) + "%"];
                     }
                 }
             },
