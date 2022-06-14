89173bee263f0fa818d814a84e9e08740b510976
Pratik <68642400+pratikbin@users.noreply.github.com>
2022-06-14 13:44:35 +0530

    fix: fix env, remove ENV after migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [[ ! -z "${DB_MIGRATION}" && "${DB_MIGRATION}" == "true" ]]; then
   DB_MIGRATION_PATH='/assetmantle/explorer/conf/evolutions/default/0.sql'
   echo "Migrating ${DB_MIGRATION_PATH}"
-  usql "postgres://${POSTGRESQL_USERNAME}:${POSTGRESQL_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT:-"5432"}/?sslmode=disable" <"${DB_MIGRATION_PATH}"
+  usql "postgres://${POSTGRES_ROOT_USERNAME}:${POSTGRES_ROOT_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT:-"5432"}/?sslmode=disable" <"${DB_MIGRATION_PATH}"
 fi
-
+unset POSTGRES_ROOT_USERNAME POSTGRES_ROOT_PASSWORD
 /assetmantle/explorer/bin/assetmantle
