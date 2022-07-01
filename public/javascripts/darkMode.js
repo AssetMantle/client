41823ad999b4ba799b8237b6d7e6e5cabc6d2ff5
avkr003 <abhinav@persistence.one>
2022-07-01 16:22:52 +0530

    default Dark Mode
    Signed-off-by: avkr003 <abhinav@persistence.one>

--- public/javascripts/darkMode.js
+++ public/javascripts/darkMode.js
@@ -1,37 +1,43 @@
 function togglePageContentLightDark() {
-    if($('body').hasClass('lightMode')){
+    let newClass;
+    let bodyDiv = $('#body')
+    if (bodyDiv.hasClass('lightMode')) {
         $('#toggleLightDark .darkMode').removeClass("hidden");
         $('#toggleLightDark .dayMode').addClass("hidden");
-    }
-    else {
+    } else {
         $('#toggleLightDark .dayMode').removeClass("hidden");
         $('#toggleLightDark .darkMode').addClass("hidden");
     }
-    var body = document.getElementById('body')
-    var currentClass = body.className
-    if ($('body').hasClass('darkMode')) {
-        var newClass ='lightMode';
-    }
-    else if ($('body').hasClass('lightMode')) {
-        var newClass ='darkMode';
+    let body = document.getElementById('body');
+    if (bodyDiv.hasClass('darkMode')) {
+        newClass = 'lightMode';
+    } else if ($('body').hasClass('lightMode')) {
+        newClass = 'darkMode';
+    } else {
+        newClass = 'darkMode';
     }
     body.className = newClass
     let date = new Date(Date.now() + 86400e3);
     date = date.toUTCString();
-    document.cookie = 'theme=' + (newClass == 'darkMode' ? 'dark' : 'light') + " ; expires=" + date;
+    let mode = (newClass === 'lightMode' ? 'light' : 'dark')
+    document.cookie = 'theme=' + (newClass === 'lightMode' ? 'light' : 'dark') + " ; expires=" + date;
 }
 
 function isDarkThemeSelected() {
     return document.cookie.match(/theme=dark/i) != null
 }
 
-function setThemeFromCookie() {
-    var body = document.getElementById('body')
-    body.className = isDarkThemeSelected() ? 'darkMode' : 'lightMode'
+function isLightThemeSelected() {
+    return document.cookie.match(/theme=light/i) != null
 }
 
-(function() {
-    setThemeFromCookie()
-    isDarkThemeSelected()
-
-})();
+$(document).ready(function () {
+    let bodyDiv = $('#body')
+    if (isDarkThemeSelected() && !isLightThemeSelected()) {
+        bodyDiv.className = 'darkMode'
+    } else if (!isDarkThemeSelected() && isLightThemeSelected()) {
+        bodyDiv.className = 'lightMode'
+    } else {
+        bodyDiv.className = 'darkMode'
+    }
+});
