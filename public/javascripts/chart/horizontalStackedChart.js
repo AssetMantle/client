671776641bfab1eda55d0192bc5cae0792bde7dc
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2023-01-17 11:41:35 +0530

    fix: transaction messages chart

--- public/javascripts/chart/horizontalStackedChart.js
+++ public/javascripts/chart/horizontalStackedChart.js
@@ -1,6 +1,27 @@
-$('.value').each(function() {
-    var text = $(this).text();
-    $(this).parent().css('width', text);
-});
+function horizontalStackedChart(id, keys, values){
+    let nameList = keys.replace('List(', '').slice(0, -1).split(', ');
+    let valueList = values.replace('Iterable(', '').replace(')', '').split(', ');
+    let totalValue = parseFloat(valueList[0]);
+    let colors = [horizontal_stacked_chart_theme_colors.block1, horizontal_stacked_chart_theme_colors.block2, horizontal_stacked_chart_theme_colors.block3, horizontal_stacked_chart_theme_colors.block4, horizontal_stacked_chart_theme_colors.block5, horizontal_stacked_chart_theme_colors.block6, horizontal_stacked_chart_theme_colors.block7, horizontal_stacked_chart_theme_colors.block8, horizontal_stacked_chart_theme_colors.block9];
 
-$('.block').tooltip();
+    for (let i = 1; i < valueList.length; i++) {
+        totalValue = totalValue + parseFloat(valueList[i]);
+    }
+
+    valueList.slice(0, 9).forEach((item,index) => {
+        $("#" + id + " .chart").append(`
+        <span class="block" title="${nameList[index]} - ${(item * 100.0 / totalValue).toFixed(2) + "%"}" style="width: ${(item * 100.0 / totalValue).toFixed(2) + "%"}; background-color: ${colors[index]};"></span>`);
+    });
+
+    let legendColumn = 1;
+    nameList.slice(0, 9).forEach((item,index) => {
+        $("#" + id + " .legend").append(`
+        <div class="legendItem section${legendColumn}">
+            <span class="legendColor" style="background-color: ${colors[index]};"></span>
+            <span class="legendLabel">${item}</span>    
+        </div>`);
+        if((index+1) % 3 === 0){
+            legendColumn+=1;
+        }
+    });
+}
--- public/javascripts/theme.js
+++ public/javascripts/theme.js
@@ -3,3 +3,15 @@ const theme_colors = {
     line : "#383838",
     disabled : "#5F5D5A",
 };
+
+const horizontal_stacked_chart_theme_colors = {
+    block1 : "#FFB400",
+    block2 : "#006BAF",
+    block3 : "#D7E9BD",
+    block4 : "#FFBFC2",
+    block5 : "#55B9A9",
+    block6 : "#D5DB48",
+    block7 : "#FF3F00",
+    block8 : "#007C7B",
+    block9 : "#542157",
+};
