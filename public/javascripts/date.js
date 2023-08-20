de1a5cfa900d1af9528b70cc842a74f808b26fa0
avkr003 <abhinav@assetmantle.one>
2023-08-20 15:27:51 +0530

    date fixes

--- public/javascripts/date.js
+++ public/javascripts/date.js
@@ -3,8 +3,13 @@ function convertUTCDateToLocalDate(date, id) {
     $('#' + id).html(newDate.toLocaleDateString("en", dateFormatOptions));
 }
 
+function fromMillisToLocal(date, id) {
+    let newDate = new Date(date);
+    $('#' + id).html(newDate.toLocaleDateString("en", dateFormatOptions));
+}
+
 function fromEpochDateToLocal(epochValue, id) {
-    let millis = epochValue;
+    let millis = epochValue * 1000;
     let dateTime = new Date(millis);
     $('#' + id).html(dateTime.toLocaleDateString("en", dateFormatOptions));
 }
