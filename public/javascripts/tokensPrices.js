e76995d9e21ba9a4b8e7330707f57e3e50567b24
Arav <aravsaraff@gmail.com>
2024-09-13 16:46:24 +0530

    Commented out components: tokensPrices, votingPower, tokensStatistics, validators, proposals, parameters, wallet

--- public/javascripts/tokensPrices.js
+++ public/javascripts/tokensPrices.js
@@ -1,12 +1,12 @@
-function showPrices(symbolList) {
-    let symbols = symbolList.replace('Set(', '').replace(')', '').split(',');
-    for (let i = 0; i < symbols.length; i++) {
-        console.log(symbols[i]);
-        $('#tokenPrices_' + symbols[i].replace(' ', '')).hide();
-    }
-    $('#tokenPrices_' + $('#tokensPricesSymbolSelect').val()).show();
-}
-
-$(document).ready(function () {
-    $('#tokenPrices_' + $('#tokensPricesSymbolSelect').val()).show();
-});
+// function showPrices(symbolList) {
+//     let symbols = symbolList.replace('Set(', '').replace(')', '').split(',');
+//     for (let i = 0; i < symbols.length; i++) {
+//         console.log(symbols[i]);
+//         $('#tokenPrices_' + symbols[i].replace(' ', '')).hide();
+//     }
+//     $('#tokenPrices_' + $('#tokensPricesSymbolSelect').val()).show();
+// }
+//
+// $(document).ready(function () {
+//     $('#tokenPrices_' + $('#tokensPricesSymbolSelect').val()).show();
+// });
